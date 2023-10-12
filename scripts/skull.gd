f8a99f6e215589448c95d57c79b724c2081f3277
Jacob Penland <jrpenlan@ucsc.edu>
2023-10-11 19:44:02 -0700

    skull remove function working

--- scripts/skull.gd
+++ scripts/skull.gd
@@ -1,5 +1,6 @@
 extends StaticBody3D
 
+var mouse_hover = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,5 +9,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("left_click"):
+	if Input.is_action_pressed("left_click") and mouse_hover:
 		$".".queue_free()
+
+
+
+func _on_mouse_entered():
+	mouse_hover = true;
+	pass # Replace with function body.
+
+
+func _on_mouse_exited():
+	mouse_hover = false;
+	pass # Replace with function body.
